9c8686982201464a9f0210e4689825aee06a139d
IvGri <110568754+IvGri@users.noreply.github.com>
2022-08-04 01:20:50 +0300

    Cleaning

--- TestApp_24hS/USimpleNumberWriterThread.pas
+++ TestApp_24hS/USimpleNumberWriterThread.pas
@@ -22,37 +22,6 @@ type
 
 implementation
 
-{
-  Important: Methods and properties of objects in visual components can only be
-  used in a method called using Synchronize, for example,
-
-      Synchronize(UpdateCaption);
-
-  and UpdateCaption could look like,
-
-    procedure TSimpleNumberWriterThread.UpdateCaption;
-    begin
-      Form1.Caption := 'Updated in a thread';
-    end;
-
-    or
-
-    Synchronize(
-      procedure
-      begin
-        Form1.Caption := 'Updated in thread via an anonymous method'
-      end
-      )
-    );
-
-  where an anonymous method is passed.
-
-  Similarly, the developer can call the Queue method with similar parameters as
-  above, instead passing another TThread class as the first parameter, putting
-  the calling thread in a queue with the other thread.
-
-}
-
 { TSimpleNumberWriterThread }
 
 procedure TSimpleNumberWriterThread.Initialize(const AThreadName: string; ACurrentNumberRef: PInteger;
@@ -65,8 +34,8 @@ end;
 
 procedure TSimpleNumberWriterThread.Execute;
 begin
-  NameThreadForDebugging('SimpleNumberWriter');
-  { Place thread code here }
+  // Get new simple number after the current one
+  // Save it to the files
 end;
 
 function TSimpleNumberWriterThread.IsSimpleNumber(
