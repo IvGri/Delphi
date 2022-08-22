b146b274dfbdc1acc69bc78465c507c132c6322a
IvGri <110568754+IvGri@users.noreply.github.com>
2022-08-22 21:51:51 +0300

    Cleaning

--- TestApp_DB_Measurer/uDataModule.pas
+++ TestApp_DB_Measurer/uDataModule.pas
@@ -3,17 +3,13 @@ unit uDataModule;
 interface
 
 uses
-  System.SysUtils, System.Classes, Data.DB, Data.Win.ADODB;
+  SysUtils, Classes, DB, ADODB;
 
 type
   TdmMain = class(TDataModule)
     ADOConnection: TADOConnection;
     ADOdsLocationsWithReadings: TADODataSet;
     dsLocationsWithReadings: TDataSource;
-  private
-    { Private declarations }
-  public
-    { Public declarations }
   end;
 
 var
